8cec7d473feb2cc289ff329723be4ea040639b2c
willisdorden <willisdorden@gmail.com>
2019-09-05 15:54:21 -0400

    built some test for the user

--- tests/integration/components/user-side-panel-test.js
+++ tests/integration/components/user-side-panel-test.js
@@ -5,22 +5,37 @@ import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | user-side-panel', function(hooks) {
   setupRenderingTest(hooks);
-
+  hooks.beforeEach( function() {
+    this.user = [{
+      name: 'john doe',
+      username: '@joe',
+      address: {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874"
+      },
+      email: 'john@gmail.com',
+      phone: '234098238',
+      website:'www.wwwwww.com',
+      company: {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+      }
+    }];
+    this.users = [{
+      user: this.get('user')
+    }];
+  });
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('users', this.users);
 
-    await render(hbs`<UserSidePanel />`);
+    await render(hbs`<UserSidePanel @user={{users}} />`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.equal(this.$('#userSidePanel').length,1);
 
-    // Template block usage:
-    await render(hbs`
-      <UserSidePanel>
-        template block text
-      </UserSidePanel>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
